fdec288abab191aef763ceb218e7d77c352e054d
Xpawn <spawn@nenufarcorps.com>
2020-02-13 03:57:44 +0100

    prepare.sh v0.21

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -4,22 +4,53 @@
 #   cgroups
 #   hugepages
 
-# ToDo
-#   Ask for user data
-
 export LC_CTYPE=C
 
 cd /tmp;
 
 # Check for root
-[[ ${EUID} = 0 ]] || { echo -e "This script should be run as root\n"; exit 2; }
+[[ ${EUID} = 0 ]] || { echo -e "This script must be run as root\n"; exit 2; }
 
 MY_USER=
+MY_EMAIL=
 SYS_LOCALE=
 SSH_TCP_PORT=
 SRV_IP=
 SRV_FQDN=
-DC_INSTALL_PATH=.dragonchain-installer/uvn-os
+
+UVN_OS_VERSION='0.21b';
+
+DC_ENV=/root/.dc-installer-env;
+DC_BASE_PATH=.dragonchain-installer/uvn-os;
+DC_INSTALL_PATH=
+SYS_BACKUP_PATH=
+
+MEMTOTAL=$(awk '/MemTotal/ { print $2 * 1024 }' /proc/meminfo);
+
+SELF_PATH=$(pwd);
+
+# Save installer env vars to disk
+function set_env()
+{
+    cat > "${DC_ENV}" <<EOL
+MY_USER=${MY_USER}
+MY_EMAIL=${MY_EMAIL}
+SYS_LOCALE=${SYS_LOCALE}
+SSH_TCP_PORT=${SSH_TCP_PORT}
+SRV_IP=${SRV_IP}
+SRV_FQDN=${SRV_FQDN}
+DC_INSTALL_PATH=${DC_INSTALL_PATH}
+SYS_BACKUP_PATH=${SYS_BACKUP_PATH}
+MEMTOTAL=${MEMTOTAL}
+UVN_OS_VERSION=${UVN_OS_VERSION}
+EOL
+}
+
+# Retrieve installer env vars from disk
+function get_env()
+{
+    [[ -f "${DC_ENV}" ]] && source "${DC_ENV}";
+}
 
 # Add new user
 function add_user()
@@ -40,7 +71,10 @@ function add_user()
     [ $? -eq 0 ] || { echo -e "\tFailed to add a user!"; exit 1; }
     
     echo -e "\tUser has been added to system!";
+
     MY_USER="${u}";
+    
+    set_env
 }
 
 # Set system locale
@@ -51,6 +85,8 @@ function set_locale()
     [[ ${#l} -eq 2 && $(locale -a | grep ^${l}_${l^^}) ]] || l="es";
     
     SYS_LOCALE=${l};
+    
+    set_env
 }
 
 # Get random number between range
@@ -83,6 +119,8 @@ function set_ssh_port()
     [[ "${p,,}" = "r" ]] && p=$(get_rnd_num 33000 62000) || { [[ ${p} =~ ^[0-9]+$ ]] && p=${p} || p=22; };
 
     SSH_TCP_PORT="${p}";
+    
+    set_env
 }
 
 # Set ip
@@ -95,9 +133,12 @@ function set_server_address()
     
     SRV_IP="${i}";
     
+    set_env
+    
     set_server_fqdn "${2}";
 }
 
+# Server FQDN
 function set_server_fqdn()
 {
     local f="${1}";
@@ -105,177 +146,273 @@ function set_server_fqdn()
     [[ $(host "${f}" | grep "${SRV_IP}") ]] || { echo -e "\tThis FQDN doesn't resolves to given address!"; exit 1; };
     
     SRV_FQDN="${f}";
+    
+    set_env
+}
+
+function set_email()
+{
+    local m="${1}";
+
+    [[ "${m}" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,11}$ ]] || { echo -e "\tThis isn't a valid e-mail address!"; exit 1; };
+    
+    MY_EMAIL="${m}";
+    
+    set_env
 }
 
 function config_complete()
 {
-    [[ -z "${MY_USER}" || -z "${SYS_LOCALE}" || -z "${SSH_TCP_PORT}" || -z "${SRV_IP}" ]] && { echo -e "Config is not complete. Try again, please."; exit 1; };
+    [[ -z "${MY_USER}" || -z "${SYS_LOCALE}" || -z "${SSH_TCP_PORT}" || -z "${MY_EMAIL}" || -z "${SRV_IP}" ]] && { echo -e "\nConfig is not complete. Try again, please ...\n"; uvn_os_setup; return; } || set_env;
 
     sys_update
 }
 
-function get_setup_vars()
+function uvn_os_setup()
 {
-# User
-    local username;
-    local password;
+    # User
+    if [[ -z "${MY_USER}" ]]; then
+        local username;
+        local password;
 
-    echo -e "\n1) Add a new user";
-    read -p "· Enter login/username (e.g. dragon): " username;
-    read -s -p "· Enter password: " password;
-    
-    add_user "${username}" "${password}";
+        echo -e "\n1) Add a new user";
+        read -p "· Enter login/username (e.g. dragon): " username;
+        read -s -p "· Enter password: " password;
+        
+        add_user "${username}" "${password}";
 
-# Locale
-    local locale;
-    
-    echo -e "\n2) Set system locale to your language";
-    read -p "· Enter a valid locale (en, es, fr, it, pt...) [es]: " locale;
+        [[ $? -eq 0 ]] || exit 1;
+    fi
 
-    set_locale "${locale-es}";
+    # Locale
+    if [[ -z "${SYS_LOCALE}" ]]; then
+        local locale;
+        
+        echo -e "\n2) Set system locale to your language";
+        read -p "· Enter a valid locale (en, es, fr, it, pt...) [es]: " locale;
 
-# Server address
-    local auto_ip="$(get_server_ip)";
-    local auto_fqdn="$(hostname -f)";
-    local ip;
-    local fqdn;
+        set_locale "${locale-es}";
 
-    echo -e "\n3) Set server public address:";
-    read -p "· Enter a public and reachable IPv4 address [${auto_ip}]: " ip;
-    read -p "· FQDN for this server (must resolve to previous IPv4) [${auto_fqdn}]: " fqdn;
-    
-    set_server_address "${ip:-$auto_ip}" "${fqdn:-$auto_fqdn}";
-    
-    echo $SRV_IP
-    echo $SRV_FQDN
+        [[ $? -eq 0 ]] || exit 1;
+    fi
 
-# SSH
-    local ssh_port;
-    
-    echo -e "\n4) Choose SSH port";
-    read -p "· Enter a number (above 1024), [r] for random or leave blank for default [22]: " ssh_port;
+    # Server address
+    if [[ -z "${SRV_IP}" ]]; then
+        local auto_ip="$(get_server_ip)";
+        local auto_fqdn="$(hostname -f)";
+        local ip;
+        local fqdn;
 
-    set_ssh_port "${ssh_port-22}";
+        echo -e "\n3) Set server public address:";
+        read -p "· Enter a public and reachable IPv4 address [${auto_ip}]: " ip;
+        read -p "· FQDN for this server (must resolve to previous IPv4) [${auto_fqdn}]: " fqdn;
+        
+        set_server_address "${ip:-$auto_ip}" "${fqdn:-$auto_fqdn}";
+        
+        [[ $? -eq 0 ]] || exit 1;
+        
+        echo $SRV_IP
+        echo $SRV_FQDN
+    fi
+
+    # SSH
+    if [[ -z "${SSH_TCP_PORT}" ]]; then
+        local ssh_port;
+        
+        echo -e "\n4) Choose SSH port";
+        read -p "· Enter a number (above 1024), [r] for random or leave blank for default [22]: " ssh_port;
+
+        set_ssh_port "${ssh_port-22}";
+    fi
+
+    # E-mail for notifications
+    if [[ -z "${MY_EMAIL}" ]]; then
+        local email;
+
+        echo -e "\n5) Server notifications & warnings";
+        read -p "· E-mail address: " email;
+
+        set_email "${email}";
+    fi
+
+    config_complete
 }
 
 function sys_update()
 {
-    apt update && apt -y full-upgrade && apt upgrade && apt autoclean && apt -y autoremove && sleep .1 && sys_tools
+    apt update && apt -y full-upgrade && apt upgrade && apt autoclean && apt -y autoremove && sleep .1;
+    
+    [[ $? -eq 0 ]] && sys_tools || exit 1;
 }
 
 function sys_tools()
 {
     apt install -y fail2ban rkhunter checksecurity dnsutils iotop \
-zram-tools screen certbot gpg dump mc ufw curl jq openssl xxd snapd && sleep .1 && rkhunter --propupd && sleep .1 && sys_config
+zram-tools screen certbot gpg dump mc ufw curl jq openssl xxd snapd && sleep .1 && rkhunter --propupd && sleep .1;
+
+    [[ $? -eq 0 ]] && sys_config || exit 1;
 }
 
 function sys_config()
 {
-    echo 'ALLOCATION=256' > /etc/default/zramswap;
-
-    echo -e "\n\npre-hook = ufw allow http\npost-hook = ufw delete allow http\n" >> /etc/letsencrypt/cli.ini;
+    [[ $(grep '^ALLOCATION=256' "/etc/default/zramswap") ]] || echo 'ALLOCATION=256' > /etc/default/zramswap;
+    
+    [[ $(grep 'ufw allow http' "/etc/letsencrypt/cli.ini") ]] || echo -e "\n\npre-hook = ufw allow http\npost-hook = ufw delete allow http\n" >> /etc/letsencrypt/cli.ini;
 
     # L10N
-    local locale="${SYS_LOCALE,,}_${SYS_LOCALE^^}";
-    local locale_alt="en_US";
+    if [[ ! $(grep 'export LANG' "/etc/profile") ]]; then
+        local locale="${SYS_LOCALE,,}_${SYS_LOCALE^^}";
+        local locale_alt="en_US";
     
-    [[ "x${SYS_LOCALE,,}" = "xen" ]] && locale_alt="es_ES";
+        [[ "x${SYS_LOCALE,,}" = "xen" ]] && locale_alt="es_ES";
 
-    echo ': "${LANG:=$locale.utf8}"; export LANG' >> /etc/profile;
-    echo -e '$locale_alt.UTF-8 UTF-8\nlocale.UTF-8 UTF-8\n' > /etc/locale.gen;
+        echo ': "${LANG:=$locale.utf8}"; export LANG' >> /etc/profile;
+        echo -e '$locale_alt.UTF-8 UTF-8\nlocale.UTF-8 UTF-8\n' > /etc/locale.gen;
 
-    cat >/etc/default/locale <<EOL
+        cat >/etc/default/locale <<EOL
 LANGUAGE="locale.UTF-8:${SYS_LOCALE,,}"
 LC_TIME="locale.UTF-8"
 LC_MESSAGES="POSIX"
 LC_COLLATE="C"
 EOL
 
-    cat > /etc/apt/apt.conf.d/999-local <<- EOT
+        locale-gen && dpkg-reconfigure tzdata;
+    fi
+
+    # Mount points
+    if [[ $(grep 'mount -o remount,exec /tmp' "/etc/apt/apt.conf.d/999-local") ]]; then
+        cat > /etc/apt/apt.conf.d/999-local <<- EOT
 DPkg::Pre-Invoke{"mount -o remount,exec /tmp";};
 DPkg::Post-Invoke {"mount -o remount,noexec /tmp";};
 EOT
-
-    sed -i '/^\/dev\/sr0/d' /etc/fstab;
-
-    locale-gen && dpkg-reconfigure tzdata;
-
+        sed -i '/^\/dev\/sr0/d' /etc/fstab;
+        sed -i 's/errors=remount-ro/rw,noatime,journal_checksum,errors=remount-ro/g'
+        
+        cp /usr/share/systemd/tmp.mount /etc/systemd/system/;
+        [[ $(grep 'Options' "/etc/systemd/system/tmp.mount" | grep 'noexec') ]] || sed -i 's/^Options=/Options=noexec,/g' /etc/systemd/system/tmp.mount;
+    fi
+    
     systemctl set-default multi-user.target;
     systemctl disable autovt@.service remote-fs.target;
+    systemctl enable tmp.mount;
 
+    # FireWall
+    [[ -z "${SSH_TCP_PORT}" ]] || ufw --force enable && ufw default allow outgoing && ufw default allow routed && ufw allow in on cni0 && ufw allow out on cni0 && ufw allow ${SSH_TCP_PORT};
+    
+    # Snap
+    [[ $(snap find core | grep '^core') ]] || snap install core;
+    [[ $(snap find microk8s | grep 'microk8s') ]]  || snap install microk8s --channel=1.18/edge --classic;
+    
+    snap alias microk8s.kubectl kubectl;
+    
     # Groups
-    groupadd -r hugetlbfs;
-    groupadd -r wheel;
+    [[ $(grep 'hugetlbfs' "/etc/group") ]] || groupadd -r hugetlbfs;
+    [[ $(grep 'wheel' "/etc/group") ]] || groupadd -r wheel;
+
     sed -i 's/^# auth\s*required\s*pam_wheel.so$/auth required pam_wheel.so/g' /etc/pam.d/su;
+
+    # User
+    if [[ ! -z "${MY_USER}" ]]; then
+        [[ $(groups ${MY_USER} | grep 'hugetlbfs') ]] || gpasswd -a ${MY_USER} hugetlbfs;
+        [[ $(groups ${MY_USER} | grep 'wheel') ]] || gpasswd -a ${MY_USER} wheel;
+        [[ $(groups ${MY_USER} | grep 'microk8s') ]] || gpasswd -a ${MY_USER} microk8s;
+
+        set_installer_files
+        
+        chmod 0750 /home/${MY_USER};
+        chown -R ${MY_USER}:${MY_USER} /home/${MY_USER};
+    fi
 }
 
-# Snap
-snap install core && snap install microk8s --channel=1.18/edge --classic;
-snap alias microk8s.kubectl kubectl;
+# System & Installer files
+function set_installer_files()
+{
+    DC_INSTALL_PATH=/home/${MY_USER}/${DC_BASE_PATH};
+    SYS_BACKUP_PATH=${DC_INSTALL_PATH}/backup;
 
-PATH=${PATH}:/snap/bin;
+    set_env
+    
+    [[ ! -d "${DC_INSTALL_PATH}" ]] && { mkdir -pm 0750 "${DC_INSTALL_PATH}"; chmod 0750 "$(dirname ${DC_INSTALL_PATH})"; }
+    [[ ! -d "${SYS_BACKUP_PATH}" ]] && mkdir -pm 0750 "${SYS_BACKUP_PATH}";
 
+    cd ${DC_INSTALL_PATH} && cp -aux ${DC_ENV} .
 
+    wget https://raw.githubusercontent.com/Xpawn/dragonchain-uvn-os/master/bin/mknode.template && chmod 0755 mknode.template;
+    
+    mkdir -pm 0750 .tmp && cd .tmp;
+    
+    # Limits (ToDo)
+    
+    # SysCtl
+    wget https://raw.githubusercontent.com/Xpawn/estaribel/master/etc/sysctl.d/999-local.conf;
+    
+    SHMMAX=$(echo ${MEMTOTAL} - 1 | bc);
+    SHMALL=$(echo "${SHMMAX} / $(getconf PAGE_SIZE)" | bc);
 
-# User
-useradd -mUG hugetlbfs,microk8s,operator,sudo,users,wheel ${MY_USER};
-passwd ${MY_USER};
-chmod 0750 /home/${MY_USER};
+    sed -i "s/# kernel.shmmax/kernel.shmmax = ${SHMMAX}/g" 999-local.conf;
+    sed -i "s/# kernel.shmall/kernel.shmall = ${SHMALL}/g" 999-local.conf;
+    chmod 0644 999-local.conf && mv 999-local.conf /etc/sysctl.d/;
+    
+    # SSH
+    wget https://raw.githubusercontent.com/Xpawn/estaribel/master/etc/ssh/sshd_config;
+    
+    sed -i "s/# ListenAddress 127\.0\.0\.1/ListenAddress 127\.0\.0\.1/g" sshd_config;
+    sed -i "s/# ListenAddress 0\.0\.0\.0/ListenAddress ${SRV_IP}/g" sshd_config;
+    sed -i "s/Port 22/Port ${SSH_TCP_PORT}/g" sshd_config;
 
-DC_BASE_PATH=/home/${MY_USER}/${DC_INSTALL_PATH};
-SYS_BACKUP_PATH=${DC_BASE_PATH}/backup;
+    cp -aux /etc/ssh/sshd_config ${SYS_BACKUP_PATH}/;
+    chmod 0644 sshd_config && mv sshd_config /etc/ssh/sshd_config;
+    
+    sleep 1;
+    
+    config_microk8s
+}
 
-[[ ! -d "${DC_BASE_PATH}" ]] && { mkdir -pm 0750 "${DC_BASE_PATH}"; chmod 0750 "$(dirname ${DC_BASE_PATH})"; }
+function mkcert()
+{
+    if [[ ! -z "${MY_EMAIL}" ]]; then
+        certbot register --agree-tos --no-eff-email -m ${MY_EMAIL};
+    fi
 
-[[ ! -d "${SYS_BACKUP_PATH}" ]] && mkdir -pm 0750 "${SYS_BACKUP_PATH}";
+    sleep 1;
 
-cd ${DC_BASE_PATH} && mkdir -pm 0750 .tmp && cd .tmp;
-
-# Kernel
-wget https://raw.githubusercontent.com/Xpawn/estaribel/master/etc/sysctl.d/999-local.conf;
-cat /proc/meminfo > meminfo;
-
-MEMTOTAL=$(awk '/MemTotal/ { print $2 * 1024 }' meminfo);
-SHMMAX=$(echo ${MEMTOTAL} - 1 | bc);
-SHMALL=$(echo "${SHMMAX} / $(getconf PAGE_SIZE)" | bc);
-
-sed -i "s/# kernel.shmmax/kernel.shmmax = ${SHMMAX}/g" 999-local.conf;
-sed -i "s/# kernel.shmall/kernel.shmall = ${SHMALL}/g" 999-local.conf;
-
-chmod 0644 999-local.conf && mv 999-local.conf /etc/sysctl.d/999-local.conf;
-
-# Limits (ToDo)
-
-# SSH
-wget https://raw.githubusercontent.com/Xpawn/estaribel/master/etc/ssh/sshd_config;
-
-sed -i "s/Port 22/Port ${SSH_TCP_PORT}/g" sshd_config;
-sed -i "s/# ListenAddress 127\.0\.0\.1/ListenAddress 127\.0\.0\.1/g" sshd_config;
-sed -i "s/# ListenAddress 0\.0\.0\.0/ListenAddress ${SRV_IP}/g" sshd_config;
-
-mv /etc/ssh/sshd_config ${SYS_BACKUP_PATH}/;
-chmod 0644 sshd_config && mv sshd_config /etc/ssh/sshd_config;
-
-# FireWall
-ufw --force enable; ufw default allow outgoing; ufw default allow routed; ufw allow in on cni0; ufw allow out on cni0; ufw allow ${SSH_TCP_PORT};
-
-# Install dir ownership
-chown -R ${MY_USER}:${MY_USER} /home/${MY_USER};
+    certbot certonly --standalone --preferred-challenges http -d ${SRV_FQDN};
+}
 
 # K8s
-su - ${MY_USER} -s /bin/bash;
+function config_microk8s()
+{
+    PATH=${PATH}:/snap/bin;
+    
+    snap alias microk8s.helm3 helm;
 
-microk8s.status --wait-ready && microk8s.enable storage && microk8s.enable dns && sleep 3;
-microk8s.enable helm3 && sleep 1 && microk8s.enable registry && sleep 2;
-
-sudo snap alias microk8s.helm3 helm;
-
-echo '{"kind":"Namespace","apiVersion":"v1","metadata":{"name":"dragonchain","labels":{"name":"dragonchain"}}}' | kubectl create -f -;
-
-helm repo add dragonchain https://dragonchain-charts.s3.amazonaws.com && helm repo update;
+    runuser -l  ${MY_USER} -c 'microk8s.status --wait-ready && microk8s.enable storage && microk8s.enable dns && sleep 3;';
+    runuser -l  ${MY_USER} -c 'microk8s.enable helm3 && sleep 1 && microk8s.enable registry && sleep 2;';
+    runuser -l  ${MY_USER} -c '/snap/bin/helm repo add dragonchain https://dragonchain-charts.s3.amazonaws.com && /snap/bin/helm repo update;';
+    
+    echo '{"kind":"Namespace","apiVersion":"v1","metadata":{"name":"dragonchain","labels":{"name":"dragonchain"}}}' | /snap/bin/kubectl create -f -;
+ 
+    show_kube;
+}
 
 # Final check
-microk8s.kubectl get nodes && microk8s.kubectl get services && microk8s.kubectl get po,svc --namespace kube-system;
+function show_kube()
+{
+    runuser -l  ${MY_USER} -c 'microk8s.kubectl get nodes && microk8s.kubectl get services && microk8s.kubectl get po,svc --namespace kube-system;';
+}
 
-# Say Good Bye!
+# Self delete
+function sayonara()
+{
+    echo -e "\nConfig done!\n\nThis script will be deleted after reboot.\n";
+    read -n 1 -spt 5 "Press [ENTER] or wait five seconds for reboot..." r;
+    echo
+    echo "rm ${SELF_PATH}/${DC_ENV}";
+    echo "rm ${SELF_PATH}/${0}";
+    
+    shutdown -R now;
+}
+
+uvn_os_setup
+
 exit 0
